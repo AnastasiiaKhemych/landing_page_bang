45f0620da6b527ab2e81670744eaaa71369567d1
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-09-12 15:16:50 +0300

    Updates

--- index.59e62017.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});// const form = document.querySelector('.form');
-//
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   form.reset();
-// });
-//# sourceMappingURL=index.59e62017.js.map
-
-//# sourceMappingURL=index.59e62017.js.map
